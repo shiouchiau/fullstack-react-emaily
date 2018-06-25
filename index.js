5cc42098ac00bee2b6c0207cb6aac63a24e27528
Steven Jan <shiouchiau@gmail.com>
2018-06-25 15:22:48 +0800

    test production dirname

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === 'production') {
     // Express will servr up index.js file if it doesn't recognize the route.
     const path = require('path');
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
